56892aa108acc9dcfbfec909cb1f07ccfd181700
AnasYasser <>
2020-09-04 13:41:45 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -4,3 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+JOB_TYPE = (
+    ('Full Time' , 'Full Time'),
+    ('Part Time' ,'Part Time'),
+)
+
+
+
+class Job(models.Model):
+    titel = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=15 , choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    
+
+    def __str__(self):
+        return self.titel
---
+++ outlin.md
@@ -0,0 +1,33 @@
+Job :
+    - title
+    - location
+    - description
+    - published at
+    - vacancy
+    - salary
+    - category
+    - experience
+    
+
+
+    - apply job
+    - post jop
+
+
+
+Blog :
+    - title
+    - description
+    - created_at
+    - category
+    - tags
+    - author
+
+    - search
+    - comment
+    - recent posts
+contact
+home
+
+
+login 
